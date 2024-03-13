1f14b82630ee8c42baa7b7774bda04db1909d861
Roberto Di Stefano <robertodistefano24@gmail.com>
2024-03-13 20:27:53 +0100

    until computeCommunityGateway

--- c++_implementation/Utility.cpp
+++ c++_implementation/Utility.cpp
@@ -8,6 +8,7 @@
 #include "Utility.h"
 
 //using namespace NetworKit::CommunityDetectionAlgorithm;
+// using namespace NetworKit::Centrality;
 using namespace NetworKit::GraphTools;
 using namespace NetworKit;
 using namespace std;
@@ -16,31 +17,76 @@ Utility::Utility(){}
 
 Utility::~Utility(){}
 
-pair<Partition*,  list<Graph*>> Utility::computeComunity(Graph* G){
+pair<Partition,  map<int, Graph>> Utility::computeComunity(Graph* G){
     PLM* plmCommunityAlgo = new PLM(*G);
     plmCommunityAlgo->run();
 
     Partition* plmCommunity = new Partition();
     *plmCommunity = plmCommunityAlgo->getPartition();
 
-    list<Graph*> community_graphs;
+
+    map<int, Graph> community_graphs;
     for(int i=0; i < plmCommunity->numberOfSubsets(); ++i){
         set<index> subset = plmCommunity->getMembers(i);
         unordered_set<index> subset_unordered  (subset.begin(), subset.end() );
         Graph community_graph = subgraphFromNodes(*G, subset_unordered);
-        community_graphs.push_back(&community_graph);
+        community_graphs[i] = community_graph;
     }
 
-    pair<Partition*,  list<Graph*>> result (plmCommunity, community_graphs );
+    pair<Partition,  map<int, Graph>> result (*plmCommunity, community_graphs );
 
     delete plmCommunityAlgo;
+    delete plmCommunity;
 
     return result;
 }
 
+pair<node, double> Utility::btwMax(Graph* graph){
+    Betweenness* LBC = new Betweenness( *graph);
+    LBC->run();
+    pair<node, double> result = LBC->ranking()[1];
+    delete LBC;
+
+    return result;
+}
+
+node Utility::computeCommunityGateway(Graph* graph,  Graph* communityGraph, set<index> communityNodes, pair<node, double> maxLBC_node ){
+    int maxICL = 0;
+    list<node> maxICL_node;
+    for(auto nodeFrom = communityNodes.begin(); nodeFrom != communityNodes.end(); nodeFrom ++ ){
+        int totalDegree = graph->degree(nodeFrom); // ToDo forse va messo il tipo count come anche nelle righe successive
+        int innerDegree = communityGraph->degree(nodeFrom);
+        int outDegree = totalDegree - innerDegree;
+        if(outDegree > maxICL){
+            maxICL_node = {nodeFrom}
+        }
+        else if(outDegree == maxICL){
+            maxICL_node.push_back(nodeFrom)
+        }
+
+        // ToDo termina funzione a partire da riga 40 dell'equivalente python
+
+    }
+
+
+}
+
 void Utility::stdImplementation(NetworKit::Graph* G){
-    pair<Partition*,  list<Graph*>> plmCommunitiesAndGraphs = Utility::computeComunity(G);
-    cout << plmCommunitiesAndGraphs.first << "end Algorithm\n";
+    pair<Partition,  map<int, Graph>> plmCommunitiesAndGraphs = Utility::computeComunity(G);
+    Partition* communitySets = &plmCommunitiesAndGraphs.first;
+    map<int, Graph> communityGraphs = plmCommunitiesAndGraphs.second;
+
+    map<int, int> maxLBC_community;
+    map<int, int> gateways;
+
+    for(auto i = communityGraphs.begin(); i != communityGraphs.end(); i ++ ){
+        pair<node, double> maxLBC_node = Utility::btwMax(&i->second);
+        maxLBC_community[i->first] = maxLBC_node;
+        //gateways[i] =
+    }
+
+
+    cout << "end Algorithm\n";
 }
 
 
--- c++_implementation/Utility.h
+++ c++_implementation/Utility.h
@@ -1,24 +1,30 @@
 #ifndef UTILITY_H_
 #define UTILITY_H_
 
+#include <networkit/Globals.hpp>
 #include <networkit/graph/Graph.hpp>
 #include <networkit/community/PLM.hpp>
 #include <networkit/structures/Partition.hpp>
 #include <networkit/graph/GraphTools.hpp>
+#include <networkit/centrality/Betweenness.hpp>
 #include <unordered_set>
 #include <list>
+#include <vector>
+#include <map>
 #include <iostream>
 #include <utility>
 
 class Utility {
     public:
         Utility();
-
+        virtual ~Utility();
         static void stdImplementation(NetworKit::Graph* G);
 
-        static std::pair<NetworKit::Partition*,  std::list<NetworKit::Graph*>> computeComunity(NetworKit::Graph* G);
+    private:
+        static std::pair<NetworKit::Partition,  std::map<int, NetworKit::Graph>> computeComunity(NetworKit::Graph* G);
+        static std::pair<NetworKit::node, double> btwMax(NetworKit::Graph* graph);
+        static NetworKit::node Utility::computeCommunityGateway(NetworKit::Graph* graph,  NetworKit::Graph* communityGraph, std::set<NetworKit::index> communityNodes, std::pair<NetworKit::node, double> maxLBC_node );
 
-        virtual ~Utility();
 
 };
 #endif
